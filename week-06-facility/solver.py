c4314b9762921bcde8763e3214a7ec0f9edbfafb
Jixin Feng <ufjfeng@users.noreply.github.com>
2017-10-29 18:25:40 -0400

    wrap trivial solution as a function

--- week-06-facility/solver.py
+++ week-06-facility/solver.py
@@ -39,31 +39,11 @@ def solve_it(input_data):
     # trivial solution
     # pack the facilities one by one until all the customers are served
     # ==========
-    # solution = [-1]*len(customers)
-    # capacity_remaining = [f.capacity for f in facilities]
-
-    # facility_index = 0
-    # for customer in customers:
-    #     if capacity_remaining[facility_index] >= customer.demand:
-    #         solution[customer.index] = facility_index
-    #         capacity_remaining[facility_index] -= customer.demand
-    #     else:
-    #         facility_index += 1
-    #         assert capacity_remaining[facility_index] >= customer.demand
-    #         solution[customer.index] = facility_index
-    #         capacity_remaining[facility_index] -= customer.demand
-
-    # calculate the cost of the solution
-    # used = [0]*len(facilities)
-    # for facility_index in solution:
-    #     used[facility_index] = 1
-
-    # obj = sum([f.setup_cost*used[f.index] for f in facilities])
-    # for customer in customers:
-    #     obj += dist(customer.location, facilities[solution[customer.index]].location)
+    # obj, opt, solution = trivial(facilities, customers)
 
     obj, opt, solution = mip(facilities, customers,
-                             verbose=False)
+                             verbose=False,
+                             time_limit=1800)
 
     # prepare the solution in the specified output format
     output_data = '%.2f' % obj + ' ' + str(opt) + '\n'
@@ -125,6 +105,31 @@ def mip(facilities, customers, verbose=False, num_threads=None, time_limit=None)
         opt = 0
 
     return total_cost, opt, soln
+
+
+def trivial(facilities, customers):
+    solution = [-1] * len(customers)
+    capacity_remaining = [f.capacity for f in facilities]
+    facility_index = 0
+    for customer in customers:
+        if capacity_remaining[facility_index] >= customer.demand:
+            solution[customer.index] = facility_index
+            capacity_remaining[facility_index] -= customer.demand
+        else:
+            facility_index += 1
+            assert capacity_remaining[facility_index] >= customer.demand
+            solution[customer.index] = facility_index
+            capacity_remaining[facility_index] -= customer.demand
+
+    used = [0]*len(facilities)
+    for facility_index in solution:
+        used[facility_index] = 1
+
+    obj = sum([f.setup_cost*used[f.index] for f in facilities])
+    for customer in customers:
+        obj += dist(customer.location, facilities[solution[customer.index]].location)
+
+    return obj, 0, solution
 
 
 if __name__ == '__main__':
