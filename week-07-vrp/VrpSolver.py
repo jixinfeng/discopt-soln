03e8b82e7fd0c75b3a96e3994ebcf532df28711c
Jixin Feng <ufjfeng@users.noreply.github.com>
2017-12-01 17:22:14 -0500

    add comment to VrpSolver

--- week-07-vrp/VrpSolver.py
+++ week-07-vrp/VrpSolver.py
@@ -10,7 +10,8 @@ class VrpSolver(object):
         self.v_ct = vehicle_count
         self.v_cap = vehicle_capacity
         self.obj = 0
-        self.tours = []
+        self.tours = self.greedy_init()
+        return
 
     def __str__(self):
         obj = self.total_tour_dist()
@@ -26,14 +27,46 @@ class VrpSolver(object):
     def dist(c1, c2):
         return math.sqrt((c1.x - c2.x) ** 2 + (c1.y - c2.y) ** 2)
 
+    def tour_demand(self, tour):
+        return sum([self.customers[i].demand for i in tour])
+
+    def have_duplicate_missing(self):
+        customers = set(range(1, len(self.customers)))
+        for tour in self.tours:
+            for c in tour[1:-1]:
+                if c not in customers:
+                    print("Duplicate")
+                    return True
+        if customers:
+            print("Missing")
+            return True
+        return False
+
     def is_valid_tour(self, tour):
-        is_valid = (self.single_tour_demand(tour) <= self.v_cap) and \
+        """
+        :param tour: list of int, tour of a single vehicle
+        :return: True if tour is valid
+
+        a tour is valid if:
+        1. total demand doesn't not exceed vehicle cap
+        2. start and end at customer[0], which is the base
+        3. does not go back to base during the tour
+        4. does not contain duplicate stop
+        """
+        is_valid = (self.tour_demand(tour) <= self.v_cap) and \
                    (tour[0] == 0) and (tour[-1] == 0) and \
                    (0 not in tour[1:-1]) and \
                    (len(set(tour[1:-1])) == len(tour[1:-1]))
         return is_valid
 
     def is_valid_soln(self):
+        """
+        :return: True if the whole solution is valid
+
+        a solution is valid if:
+        1. every tour is valid
+        2. (TODO)no duplicate/missing customer
+        """
         return all([self.is_valid_tour(tour) for tour in self.tours])
 
     def single_tour_dist(self, tour):
@@ -41,21 +74,20 @@ class VrpSolver(object):
             return math.inf
         tour_dist = 0
         for i in range(1, len(tour)):
-            tour_dist += self.dist(self.customers[tour[i - 1]], self.customers[tour[i]])
+            customer_1 = self.customers[tour[i - 1]]
+            customer_2 = self.customers[tour[i]]
+            tour_dist += self.dist(customer_1, customer_2)
         return tour_dist
 
     def every_tour_dists(self):
-        return {v: self.single_tour_dist(tour) for v, tour in enumerate(self.tours)}
+        return [self.single_tour_dist(tour) for tour in self.tours]
 
     def total_tour_dist(self):
         dists = self.every_tour_dists()
-        if math.inf in dists.values():
+        if math.inf in dists:
             raise ValueError("Invalid tour detected.")
         else:
-            return sum(dists.values())
-
-    def single_tour_demand(self, tour):
-        return sum([self.customers[i].demand for i in tour])
+            return sum(dists)
 
     def greedy_init(self):
         tours = []
@@ -79,6 +111,19 @@ class VrpSolver(object):
             return self.tours
 
     def shift(self, i_from, start_from, end_from, i_to, j_to, debug=False):
+        """
+        :param i_from: index of tour shift from
+        :param start_from: start index of segment
+        :param end_from: end index of segment (inclusive)
+        :param i_to: index of tour shift to
+        :param j_to: location
+        :param debug: print details if True
+        :return: True if improved
+
+        shift a segment of tour into another tour
+        2 possible ways:
+        shift directly and reverse after shift
+        """
         tour_from_old = self.tours[i_from]
         tour_to_old = self.tours[i_to]
         improved = False
@@ -103,6 +148,7 @@ class VrpSolver(object):
 
         dist_from_old = self.single_tour_dist(tour_from_old)
         dist_to_old = self.single_tour_dist(tour_to_old)
+
         dist_from_new = self.single_tour_dist(tour_from_new)
 
         dist_to_new_1 = self.single_tour_dist(tour_to_new_1)
@@ -119,18 +165,34 @@ class VrpSolver(object):
         if obj_new_1 < self.obj:
             self.tours[i_from] = tour_from_new
             self.tours[i_to] = tour_to_new_1
-            self.obj = obj_new_1
+            # self.obj = obj_new_1
+            self.obj = self.total_tour_dist()
             improved = True
 
         if obj_new_2 < self.obj:
             self.tours[i_from] = tour_from_new
             self.tours[i_to] = tour_to_new_2
-            self.obj = obj_new_2
+            # self.obj = obj_new_2
+            self.obj = self.total_tour_dist()
             improved = True
 
         return improved
 
     def interchange(self, i_1, start_1, end_1, i_2, start_2, end_2, debug=False):
+        """
+        :param i_1:
+        :param start_1:
+        :param end_1:
+        :param i_2:
+        :param start_2:
+        :param end_2:
+        :param debug:
+        :return:
+
+        interchange 2 segments from 2 tours
+        4 possible ways:
+        interchange directly, reverse either segment, reverse both segments
+        """
         tour_1_old = self.tours[i_1]
         tour_2_old = self.tours[i_2]
         improved = False
@@ -180,30 +242,44 @@ class VrpSolver(object):
         if new_obj_1 < self.obj:
             self.tours[i_1] = tour_1_new_1
             self.tours[i_2] = tour_2_new_1
-            self.obj = new_obj_1
+            # self.obj = new_obj_1
+            self.obj = self.total_tour_dist()
             improved = True
 
         if new_obj_2 < self.obj:
             self.tours[i_1] = tour_1_new_1
             self.tours[i_2] = tour_2_new_2
-            self.obj = new_obj_2
+            # self.obj = new_obj_2
+            self.obj = self.total_tour_dist()
             improved = True
 
         if new_obj_3 < self.obj:
             self.tours[i_1] = tour_1_new_2
             self.tours[i_2] = tour_2_new_1
-            self.obj = new_obj_3
+            # self.obj = new_obj_3
+            self.obj = self.total_tour_dist()
             improved = True
 
         if new_obj_4 < self.obj:
             self.tours[i_1] = tour_1_new_2
             self.tours[i_2] = tour_2_new_2
-            self.obj = new_obj_4
+            # self.obj = new_obj_4
+            self.obj = self.total_tour_dist()
             improved = True
 
         return improved
 
     def exchange(self, i, start, end, debug=False):
+        """
+        :param i:
+        :param start:
+        :param end:
+        :param debug:
+        :return:
+
+        reverse a segment of a tour
+        only 1 way to do this
+        """
         improved = False
         tour_old = self.tours[i]
         seg = tour_old[start: end + 1]
@@ -220,18 +296,31 @@ class VrpSolver(object):
         new_obj = self.obj - self.single_tour_dist(tour_old) + self.single_tour_dist(tour_new)
         if new_obj < self.obj:
             self.tours[i] = tour_new
-            self.obj = new_obj
+            # self.obj = new_obj
+            self.obj = self.total_tour_dist()
             improved = True
         return improved
 
     def ladder(self, i_1, i_2, j_1, j_2, debug=False):
+        """
+        :param i_1:
+        :param i_2:
+        :param j_1:
+        :param j_2:
+        :param debug:
+        :return:
+
+        split two tours into head and tail respectively, and re-shuffle them
+        2 possible ways
+        """
         tour_1_old = self.tours[i_1]
         tour_2_old = self.tours[i_2]
         improved = False
-        print("".join(["-"] * 10))
-        print("old tours")
-        print(tour_1_old)
-        print(tour_2_old)
+        if debug:
+            print("".join(["-"] * 10))
+            print("old tours")
+            print(tour_1_old)
+            print(tour_2_old)
 
         seg_1_head = tour_1_old[:j_1]
         seg_1_tail = tour_1_old[j_1:]
@@ -245,11 +334,12 @@ class VrpSolver(object):
         # head + head(reversed) / tail(reversed) + tail
         tour_1_new_2 = seg_1_head + seg_2_head[::-1]
         tour_2_new_2 = seg_1_tail[::-1] + seg_2_tail
-        print("new tours")
-        print("{}|{}".format(seg_1_head, seg_2_tail))
-        print("{}|{}".format(seg_2_head, seg_1_tail))
-        print("{}|{}".format(seg_1_head, seg_2_head[::-1]))
-        print("{}|{}".format(seg_1_tail[::-1], seg_2_tail))
+        if debug:
+            print("new tours")
+            print("{}|{}".format(seg_1_head, seg_2_tail))
+            print("{}|{}".format(seg_2_head, seg_1_tail))
+            print("{}|{}".format(seg_1_head, seg_2_head[::-1]))
+            print("{}|{}".format(seg_1_tail[::-1], seg_2_tail))
 
         # old tour lengths
         dist_1_old = self.single_tour_dist(tour_1_old)
@@ -267,68 +357,79 @@ class VrpSolver(object):
         if new_obj_1 < self.obj:
             self.tours[i_1] = tour_1_new_1
             self.tours[i_2] = tour_2_new_1
-            self.obj = new_obj_1
+            # self.obj = new_obj_1
+            self.obj = self.total_tour_dist()
             improved = True
 
         if new_obj_2 < self.obj:
             self.tours[i_1] = tour_1_new_2
             self.tours[i_2] = tour_2_new_2
-            self.obj = new_obj_2
+            # self.obj = new_obj_2
+            self.obj = self.total_tour_dist()
             improved = True
 
         return improved
 
-    def solve(self, verbose=False):
-        self.greedy_init()
+    def solve(self,
+              shift=True,
+              interchange=True,
+              exchange=True,
+              ladder=True,
+              debug=False):
         improved = True
         while improved:
             improved = False
             self.obj = self.total_tour_dist()
-            if verbose:
+            if debug:
                 print(self.obj)
 
             # try shift
-            for i_from, tour_from in enumerate(self.tours):
-                if improved: break
-                for start_from, end_from in itertools.combinations(range(1, len(tour_from) - 1), 2):
+            if shift:
+                for i_from, tour_from in enumerate(self.tours):
                     if improved: break
-                    for i_to, tour_to in enumerate(self.tours):
+                    for start_from, end_from in itertools.combinations(range(1, len(tour_from) - 1), 2):
                         if improved: break
-                        if i_from == i_to: continue
-                        for j_to in range(1, len(tour_to) - 1):
-                            if self.shift(i_from, start_from, end_from, i_to, j_to):
-                                improved = True
-                                break
+                        for i_to, tour_to in enumerate(self.tours):
+                            if improved: break
+                            if i_from == i_to: continue
+                            for j_to in range(1, len(tour_to) - 1):
+                                if self.shift(i_from, start_from, end_from, i_to, j_to, debug):
+                                    improved = True
+                                    break
 
             # try interchange
-            for i_1, tour_1 in enumerate(self.tours):
-                if improved: break
-                for start_1, end_1 in itertools.combinations(range(1, len(tour_1) - 1), 2):
+            if interchange:
+                for i_1, tour_1 in enumerate(self.tours):
                     if improved: break
-                    for i_2, tour_2 in enumerate(self.tours):
+                    for start_1, end_1 in itertools.combinations(range(1, len(tour_1) - 1), 2):
                         if improved: break
-                        if i_1 == i_2: continue
-                        for start_2, end_2 in itertools.combinations(range(1, len(tour_2) - 1), 2):
-                            if self.interchange(i_1, start_1, end_1, i_2, start_2, end_2):
-                                improved = True
-                                break
+                        for i_2, tour_2 in enumerate(self.tours):
+                            if improved: break
+                            if i_1 == i_2: continue
+                            for start_2, end_2 in itertools.combinations(range(1, len(tour_2) - 1), 2):
+                                if self.interchange(i_1, start_1, end_1, i_2, start_2, end_2, debug):
+                                    improved = True
+                                    break
 
             # try exchange
-            for i, tour in enumerate(self.tours):
-                for start, end in itertools.combinations(range(1, len(tour) - 1), 2):
-                    if self.exchange(i, start, end):
-                        improved = True
-                        break
+            if exchange:
+                for i, tour in enumerate(self.tours):
+                    for start, end in itertools.combinations(range(1, len(tour) - 1), 2):
+                        if self.exchange(i, start, end, debug):
+                            improved = True
+                            break
 
             # try ladder
-            for i_1, tour_1 in enumerate(self.tours):
-                if improved: break
-                for j_1 in range(1, len(tour_1) - 1):
+            if ladder:
+                for i_1, tour_1 in enumerate(self.tours):
                     if improved: break
-                    for i_2, tour_2 in enumerate(self.tours):
+                    for j_1 in range(2, len(tour_1) - 2):
                         if improved: break
-                        for j_2 in range(1, len(tour_2) - 1):
-                            if self.ladder(i_1, i_2, j_1, j_2):
-                                improved = True
-                                break
+                        for i_2, tour_2 in enumerate(self.tours):
+                            if i_1 == i_2: continue
+                            if improved: break
+                            for j_2 in range(2, len(tour_2) - 2):
+                                if self.ladder(i_1, i_2, j_1, j_2, debug):
+                                    improved = True
+                                    break
         return self.tours
